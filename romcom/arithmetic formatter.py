cb21a86f10452ebbec54a24f76bcbbc4593a739e
Chioma Adande <chiomadande@gmail.com>
2024-10-24 20:55:45 +0100

    second commit

--- romcom/arithmetic formatter.py
+++ romcom/arithmetic formatter.py
@@ -1,9 +1,18 @@
 def formatter(eqns):
-    
+    answer=''
     for eqn in eqns:
         if len(eqn.split())>3:
             print("Only two numbers can be used!")
             return "Only two numbers can be used!"
         first,sign,last=eqn.split()
-        print(first)
-formatter(["45 + 9", "30 + 50", "39 - 10"])
+        if sign not in ['+', '-']:
+            print("Only addition and Subtraction allowed")
+            return "Only addition and Subtraction allowed"
+        if sign=='+':
+            answer=str(int(first)+int(last))
+            print(answer)
+        elif sign=='-':
+        
+            answer=str(int(first)-int(last))
+            print(answer)
+formatter(["45 + 9", "30 - 50", "39 - 10"])
